qbarnes/gw2dmk

commit fa601b8204bd9439984928e696dec41557334965

Change return value types for dmk read/write functions.

dmk.c
@@ -31,10 +31,10 @@ dmk_header_init(struct dmk_header *dmkh,
 
 
 /*
- * Should return 7 when all seven header items read correctly.
+ * Returns 1 when header read correctly, 0 on failure.
  */
 
-size_t
+bool
 dmk_header_fread(struct dmk_header *dmkh, FILE *fp)
 {
 	size_t	frsz = 0;
@@ -58,15 +58,15 @@ dmk_header_fread(struct dmk_header *dmkh, FILE *fp)
 
 	dmkh->real_format = le32toh(format);
 
-	return frsz;
+	return frsz == 7 ? true : false;
 }
 
 
 /*
- * Should return 7 when all seven header items written correctly.
+ * Returns 1 when header written correctly, 0 on failure.
  */
 
-size_t
+bool
 dmk_header_fwrite(const struct dmk_header *dmkh, FILE *fp)
 {
 	size_t	fwsz = 0;
@@ -87,7 +87,7 @@ dmk_header_fwrite(const struct dmk_header *dmkh, FILE *fp)
 
 	fwsz += fwrite(&format, sizeof(format), 1, fp);
 
-	return fwsz;
+	return fwsz == 7 ? true : false;
 }
 
 
@@ -109,28 +109,37 @@ dmk_track_fseek(struct dmk_header *dmkh,
 }
 
 
-size_t
+/*
+ * Returns 1 when track is read correctly, 0 on failure.
+ */
+
+bool
 dmk_track_fread(const struct dmk_header *dmkh,
 		struct dmk_track *trk,
 		FILE *fp)
 {
-	size_t	fwsz = 0;
+	size_t	frsz = 0;
 
 	for (int i = 0; i < DMK_MAX_SECTORS; ++i) {
 		uint16_t	idam_off;
 
-		// XXX error checking
-		fwsz += fread(&idam_off, sizeof(idam_off), 1, fp);
+		frsz += fread(&idam_off, sizeof(idam_off), 1, fp);
 
 		trk->idam_offset[i] = le16toh(idam_off);
 	}
 
-	return fwsz + fread(trk->track + DMK_TKHDR_SIZE,
+	frsz += fread(trk->track + DMK_TKHDR_SIZE,
 				dmkh->tracklen - DMK_TKHDR_SIZE, 1, fp);
+
+	return frsz == (DMK_MAX_SECTORS + 1) ? true : false;
 }
 
 
-size_t
+/*
+ * Returns 1 when track is written correctly, 0 on failure.
+ */
+
+bool
 dmk_track_fwrite(const struct dmk_header *dmkh,
 		 const struct dmk_track *trk,
 		 FILE *fp)
@@ -144,8 +153,10 @@ dmk_track_fwrite(const struct dmk_header *dmkh,
 		fwsz += fwrite(&idam_off, sizeof(idam_off), 1, fp);
 	}
 
-	return fwsz + fwrite(trk->track + DMK_TKHDR_SIZE,
+	fwsz += fwrite(trk->track + DMK_TKHDR_SIZE,
 				dmkh->tracklen - DMK_TKHDR_SIZE, 1, fp);
+
+	return fwsz == (DMK_MAX_SECTORS + 1) ? true : false;
 }
 
 

dmk.h
@@ -161,19 +161,19 @@ extern void dmk_track_stats_init(struct dmk_track_stats *dts);
 extern void dmk_header_init(struct dmk_header *dmkh, uint8_t tracks,
 			    uint16_t tracklen);
 
-extern size_t dmk_header_fread(struct dmk_header *dmkh, FILE *fp);
+extern bool dmk_header_fread(struct dmk_header *dmkh, FILE *fp);
 
-extern size_t dmk_header_fwrite(const struct dmk_header *dmkh, FILE *fp);
+extern bool dmk_header_fwrite(const struct dmk_header *dmkh, FILE *fp);
 
 extern long dwk_track_file_offset(struct dmk_header *dmkh, int track, int side);
 
 int dmk_track_fseek(struct dmk_header *dmkh, int track, int side, FILE *fp);
 
-extern size_t dmk_track_fread(const struct dmk_header *dmkh,
-			      struct dmk_track *trk, FILE *fp);
+extern bool dmk_track_fread(const struct dmk_header *dmkh,
+			    struct dmk_track *trk, FILE *fp);
 
-extern size_t dmk_track_fwrite(const struct dmk_header *dmkh,
-			       const struct dmk_track *trk, FILE *fp);
+extern bool dmk_track_fwrite(const struct dmk_header *dmkh,
+			     const struct dmk_track *trk, FILE *fp);
 
 extern void dmk_data_rotate(struct dmk_track *trk, uint8_t *data_hole);
 

commit 8a3f64fdc8bda870301f9c2d6be672ba82b49d05

Fix problem introduced by fa601b8.

dmk.c
@@ -265,7 +265,7 @@ fp2dmk(FILE *fp, struct dmk_file *dmkf)
 	fseek(fp, 0, SEEK_SET);  // XXX Do this here?
 
 	size_t hret = dmk_header_fread(&dmkf->header, fp);
-	if (hret != 7)
+	if (hret != 1)
 		return -1;
 
 	/* Sanity check. */